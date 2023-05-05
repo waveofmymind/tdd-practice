46ebd1277f0897fb4a567ebb80382da26daa3fda
waveofmymind <sjun0913@gmail.com>
2023-05-05 14:23:55 +0900

    Init Commit

---
+++ cfd/src/main/java/wave/cfd/CfdApplication.java
@@ -0,0 +1,13 @@
+package wave.cfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfdApplication.class, args);
+	}
+
+}
